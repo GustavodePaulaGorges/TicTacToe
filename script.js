0d2de6d4f626e445f9f5eeb925fac104d9ee9fc6
GustavoDePaulaGorges <gustavodep.gorges@gmail.com>
2025-08-30 14:29:17 -0300

    Refatorada lógica de vitória, adicionada escalabilidade do board

--- script.js
+++ script.js
@@ -1,84 +1,182 @@
-// 2 bugs: quando ganha na ultima jogada ele dá o alert de ganhar E o de empate; texto que fala qual o jogador não é iniciado.
-
-var isGreen = (Math.random()) > 0.5; // determina quem começa
-var gameBoardWidth = 3;
+var isCircle = (Math.random()) > 0.5; // determina quem começa
+var gameBoardSize = 3;
 var posMarked = 0
-var winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
-var positions = ["", "", "", "", "", "", "", "", ""]
+
+/**
+ * TODO:
+ * fazer sistema de skin: céu azul, caderno, grama
+ *      - ceu azul: x = sol, o = nuvem
+ *      - grama: x = bixo-pau, o = joaninha
+ */
+
 var currentPlayer = document.getElementById("player")
 
-changePlayer() // meio bucha ne
+function getCurrentSymbol() {
+    return isCircle ? "circle" : "cross"
+}
+
+function initializeBoard() {
+    const gameBoard = document.getElementById("game-board");
+    let count = 0;
+
+    gameBoard.innerHTML = '';
+
+    for (let i = 0; i < gameBoardSize; i++) {
+        const newRow = document.createElement("tr")
+
+        for (let j = 0; j < gameBoardSize; j++) {
+            const newCollumn = document.createElement("td")
+
+            newCollumn.onclick = function () {
+                selectSquare(this);
+            };
+            newCollumn.id = count;
+            count++
+
+            newRow.appendChild(newCollumn)
+        }
+
+        gameBoard.appendChild(newRow)
+    }
+
+    posMarked = 0
+}
+
+function initializeGame() {
+    setPlayer(getCurrentSymbol())
+    initializeBoard()
+}
+
+function changeBoardSize(isIncrease) {
+    isIncrease ? gameBoardSize++ : gameBoardSize--
+
+    if (gameBoardSize < 1) {
+        gameBoardSize = 1
+    }
+
+    initializeBoard()
+}
 
 function selectSquare(clickedSquare) {
     if (clickedSquare.className === "") {
         markSquare(clickedSquare)
         checkWinCondition()
         changePlayer()
-        
     } else {
         alert("This position has been already marked")
     }
 }
 
 function changePlayer() {
-    let color = isGreen ? "green" : "red"
-    isGreen = !isGreen
+    isCircle = !isCircle
 
-    currentPlayer.textContent = `Current Player: ${color}`
+    setPlayer(getCurrentSymbol())
+}
+
+function setPlayer() {
+    currentPlayer.textContent = `Current Player: ${getCurrentSymbol()}`
 }
 
 function markSquare(clickedSquare) {
-    let color = isGreen ? "green" : "red"
-
-    clickedSquare.className = color;
-    positions[parseInt(clickedSquare.id)] = clickedSquare.className
+    clickedSquare.className = getCurrentSymbol();
 
     posMarked++
 }
 
 function checkWinCondition() {
-    let color = isGreen ? "green" : "red"
+    const gameBoard = document.getElementById("game-board");
+    const rows = gameBoard.children;
+    let winHorPoints = 0;
+    let winVerPoints = 0;
+    let winDiaLeftPoints = 0;
+    let winDiaRightPoints = 0;
+    let shouldCheckCols = true;
 
-    winConditions.forEach((winCon) => {
-        square1 = positions[winCon[0]]\
-        square2 = positions[winCon[1]]
-        square3 = positions[winCon[2]]
+    for (let i = 0; i < rows.length; i++) {
+        let row = rows[i]
+        let cols = row.children
 
-        if (square1 === "" || square2 === "" || square3 === "") {
-            return;
+        for (let j = 0; j < cols.length; j++) {
+            let col = cols[j];
+            let diaLeftI = 0;
+            let diaRightI = gameBoardSize - 1;
+
+            if (col.className == getCurrentSymbol()) {
+                winHorPoints++
+            }
+
+            if (winHorPoints == gameBoardSize) {
+                winAlert()
+                return;
+            }
+
+            if (shouldCheckCols) {
+                for (let h = 0; h < rows.length; h++) {
+                    let verRow = rows[h]
+                    let verCol = verRow.children[j]
+                    let diaRigthtCol = verRow.children[diaRightI]
+                    let diaLeftCol = verRow.children[diaLeftI]
+
+                    if (verCol.className == getCurrentSymbol()) {
+                        winVerPoints++
+                    }
+
+                    if (diaRigthtCol && diaRigthtCol.className == getCurrentSymbol()) {
+                        winDiaRightPoints++
+                    }
+
+                    if (diaLeftCol && diaLeftCol.className == getCurrentSymbol()) {
+                        winDiaLeftPoints++
+                    }
+
+                    if (
+                        winVerPoints === gameBoardSize ||
+                        winDiaRightPoints === gameBoardSize ||
+                        winDiaLeftPoints === gameBoardSize
+                    ) {
+                        winAlert();
+                        return;
+                    }
+
+                    diaLeftI++
+                    diaRightI--
+                }
+            }
+
+            shouldCheckCols = false
+            winVerPoints = 0
+            winDiaRightPoints = 0
+            winDiaLeftPoints = 0
         }
 
-        if (square1 === square2 && square2 === square3) {
-            winAlert(color)
-        }
-    });
+        rowBuf = row;
+        winHorPoints = 0
+    }
 
-    if (posMarked === (gameBoardWidth ** 2)) {
-        drawAlert();
+    if (posMarked == gameBoardSize ** 2) {
+        drawAlert()
+        return;
     }
 }
 
-function winAlert(color) {
-    alert(`The color ${color} has won!`);
-
+function timeoutReload() {
     setTimeout(function () {
         window.location.reload();
     }, 2000);
+}
+
+function winAlert() {
+    alert(`${getCurrentSymbol()} has won!`);
+    timeoutReload()
+    
 }
 
 function drawAlert() {
     alert("There is a draw!")
-
-    setTimeout(function () {
-        window.location.reload();
-    }, 2000);
+    timeoutReload()
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    initializeGame()
+});
